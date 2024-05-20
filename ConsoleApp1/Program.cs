2a059680b9bd40ae0ef05e7f77608f443c94eec7
MehmetSalimBingol <salim.bingol@hotmail.com>
2024-05-20 21:13:57 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string original = "The weather is so sunny nowadays";
+        string reversed = ReverseString(original);
+        Console.WriteLine(reversed);
+    }
+
+    public static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        int left = 0;
+        int S = charArray.Length;
+        int right = S - 1;
+        while (left < right)
+        {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+        return new string(charArray);
+    }
+}
+
